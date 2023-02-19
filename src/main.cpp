e2e850abd2d91f03787f9a238013ab814c153e62
Peter Rolfe <30848697+pjhrolfe@users.noreply.github.com>
2023-02-19 13:19:26 +0000

    Changed window and renderer from global to local variables

--- src/main.cpp
+++ src/main.cpp
@@ -8,25 +8,21 @@ const uint8_t FRAME_INTERVAL = 17;
 const size_t SCREEN_WIDTH = 1024;
 const size_t SCREEN_HEIGHT = 512;
 
-// Global variables
-SDL_Window* gWindow;
-SDL_Renderer* gRenderer;
-
-void initialization() {
+void initialize_window_and_renderer(SDL_Window** window, SDL_Renderer** renderer) {
     if (SDL_Init(SDL_INIT_VIDEO < 0)) {
         std::cout << "SDL video system could not be initialized: " << SDL_GetError();
     } else {
         std::cout << "SDL video system initialized" << std::endl;
     }
 
-    gWindow = SDL_CreateWindow("mini-fps",
+    *window  = SDL_CreateWindow("mini-fps",
             0,
             0,
             SCREEN_WIDTH,
             SCREEN_HEIGHT,
             SDL_WINDOW_SHOWN);
 
-    gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
+    *renderer = SDL_CreateRenderer(*window, -1, SDL_RENDERER_ACCELERATED);
 }
 
 void handle_input(bool& drawSquare, bool& gameIsRunning, int& x, int& y) {
@@ -46,28 +42,28 @@ void handle_input(bool& drawSquare, bool& gameIsRunning, int& x, int& y) {
 }
 
 void draw(SDL_Renderer* renderer, const bool drawSquare, const int x, const int y) {
-    SDL_SetRenderDrawColor(gRenderer, 255, 0, 0, SDL_ALPHA_OPAQUE);
-    SDL_RenderClear(gRenderer);
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(renderer);
         
     if (drawSquare) {
-        SDL_SetRenderDrawColor(gRenderer, 255, 255, 255, 255);
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 
         SDL_Rect rect;
         rect.x = x-12;
         rect.y = y-12;
         rect.w = 24;
         rect.h = 24;
-        SDL_RenderFillRect(gRenderer, &rect);
+        SDL_RenderFillRect(renderer, &rect);
     }
 
-    SDL_RenderPresent(gRenderer);     
+    SDL_RenderPresent(renderer);     
 }
 
 int main() {
-    gWindow = nullptr;
-    gRenderer = nullptr;
+    SDL_Window* window = nullptr;
+    SDL_Renderer* renderer = nullptr;
 
-    initialization();
+    initialize_window_and_renderer(&window, &renderer);
 
     bool drawSquare = false;
     bool gameIsRunning = true;
@@ -75,13 +71,13 @@ int main() {
     
     while (gameIsRunning) {
         handle_input(drawSquare, gameIsRunning, x, y);
-        draw(gRenderer, drawSquare, x, y);
+        draw(renderer, drawSquare, x, y);
 
         // TODO: Better frame timing solution
         SDL_Delay(FRAME_INTERVAL);   
     }
 
-    SDL_DestroyWindow(gWindow);
+    SDL_DestroyWindow(window);
     SDL_Quit();
 
     return 0;
