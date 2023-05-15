c5eb4a53cc4b0eab017b95b07ac27aeb87d928aa
Peter Rolfe <30848697+pjhrolfe@users.noreply.github.com>
2023-05-15 16:58:42 +0100

    Stripped out debug writing to file

--- src/main.cpp
+++ src/main.cpp
@@ -14,29 +14,21 @@
 #include "Menu.h"
 
 int main() {
-    ClearFile("log.txt");
-
     if (!InitializeSDL()) {
-        WriteLineToFile("log.txt", "SDL could not be initialized");
         std::cerr << "SDL could not be initialized:" << SDL_GetError();
     } else {
-        WriteLineToFile("log.txt", "SDL initialized");
         std::cout << "SDL initialized" << std::endl;
     }
 
     if (!InitializeSDLImage()) {
-        WriteLineToFile("log.txt", "SDL_image could not be initialized");
         std::cerr << "SDL_image could not be initialized" << std::endl;
     } else {
-        WriteLineToFile("log.txt", "SDL_image initialized");
         std::cout << "SDL_image initialized" << std::endl;
     }
 
     if (!InitializeSDLTTF()) {
-        WriteLineToFile("log.txt", "SDL_ttf could not be initialized");
         std::cerr << "SDL_ttf could not be initialized" << std::endl;
     } else {
-        WriteLineToFile("log.txt", "SDL_ttf initialized");
         std::cout << "SDL_ttf initialized" << std::endl;
     }
 
@@ -45,29 +37,19 @@ int main() {
 
     Settings settings = Settings::LoadSettings(GetSDLAssetsFolderPath(), "settings.json");
 
-    WriteLineToFile("log.txt", "Settings loaded");
-
     Font fonts[settings.fontPaths.size()];
     for (int i = 0; i < settings.fontPaths.size(); i++) {
         fonts[i] = Font(settings.fontPaths[i].first, GetSDLAssetsFolderPath() + settings.fontPaths[i].second, 24);
     }
 
-    WriteLineToFile("log.txt", "Fonts loaded");
-
     Texture textures[settings.texturePaths.size()];
     for (int i = 0; i < settings.texturePaths.size(); i++) {
-        WriteLineToFile("log.txt", "Texture path: " + settings.texturePaths[i].second);
         textures[i] = Texture(settings.texturePaths[i].first, GetSDLAssetsFolderPath() + settings.texturePaths[i].second);
-        std::cout << textures[i].name << std::endl;
     }
 
-    WriteLineToFile("log.txt", "Textures loaded");
-
     if (!InitializeWindowAndRenderer(&window, &renderer, settings.screenWidth, settings.screenHeight, settings.vSync)) {
-        WriteLineToFile("log.txt", "Window and/or renderer could not be initialized");
         std::cerr << "Window and/or renderer could not be initialized" << std::endl;
     } else {
-        WriteLineToFile("log.txt", "Window and renderer initialized");
         std::cout << "Window and renderer initialized" << std::endl;
     }
 
@@ -77,13 +59,9 @@ int main() {
     SDL_Texture* renderFrameTexture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_TARGET,
                                                         static_cast<int>(settings.screenWidth), static_cast<int>(settings.screenHeight));
 
-    WriteLineToFile("log.txt", "Textures created");
-
     // Allow movement of cursor in menu
     SDL_SetRelativeMouseMode(SDL_FALSE);
     MainMenu mainMenu(settings);
-
-    WriteLineToFile("log.txt", "Main menu object created");
 
     while (mainMenu.player.InMainMenu() && !mainMenu.player.GameHasEnded()) {
         DrawMainMenu(settings, renderer, fonts[0], mainMenu.player.camera, streamingFrameTexture, renderFrameTexture);
