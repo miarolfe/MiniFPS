379b79bcef12d932221b3ce2b0a7457dee1082ef
Peter Rolfe <30848697+pjhrolfe@users.noreply.github.com>
2023-06-09 23:39:32 +0100

    Documents Player in Doxygen style

--- src/Player.h
+++ src/Player.h
@@ -13,24 +13,50 @@ namespace MiniFPS {
     };
 
     struct Player {
-        Player(Level* level, Settings settings);
         Player();
+        Player(Level* level, Settings settings);
 
         Level* level;
         Camera camera;
         InputState inputState;
 
+        /**
+         * Gets input from the player.
+         */
         void UpdateInputState();
+
+        /**
+         * Applies input from the frame to the player and moves them.
+         * @param frameDelta The time between frames in seconds.
+         * @param speedModifier How fast the player moves.
+         */
         void Move(float frameDelta, float speedModifier);
+
+        /**
+         * Applies input from the frame to the player and turns them.
+         * @param frameDelta The time between frames in seconds.
+         * @param rotationModifier How fast the player turns.
+         */
         void Rotate(float frameDelta, float rotationModifier);
+
+        /**
+         *
+         * @param frameDelta The time between frames in seconds.
+         * @param speedModifier How fast the player moves.
+         * @param rotationModifier How fast the player turns.
+         */
         void Update(float frameDelta, float speedModifier, float rotationModifier);
+
+        /**
+         * Checks whether the game has ended.
+         * @return Whether the game has ended.
+         */
         bool GameHasEnded();
+
+        /**
+         * Checks if the game is on the main menu.
+         * @return Whether the game is on the main menu.
+         */
         bool InMainMenu();
     };
-
-    void GetInputState(bool &gameIsRunning, bool &moveLeft, bool &moveRight, bool &moveForward,
-                       bool &moveBack, int &mouseX, int &mouseY);
-
-    void Move(Camera &camera, float frameDelta, float speedModifier, bool moveLeft, bool moveRight,
-              bool moveForward, bool moveBack);
 }
